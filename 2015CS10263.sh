4748a4904e40cf00bb678428915bd6b631aaa337
swapnil <dasswapnil96@gmail.com>
2018-08-22 19:57:50 +0530

    Added instructions

--- 2015CS10263.sh
+++ 2015CS10263.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-./apriori dataset.dat 1 output.txt
+ap="-apriori"
+fp="-fptree"
+pl="-plot"
+
+if [ "$2" == "$pl" ]
+then
+    rm logs.txt
+    for x in {1,5,10,25,50,90}
+    do
+        echo $x
+        echo -n "$(./apriori $1 $x temp 1)" >> logs.txt 
+        echo -n " " >> logs.txt
+        echo -n "$(./fptree $1 $x temp 1)" >> logs.txt
+        echo "" >> logs.txt 
+    done
+    python plot.py
+else
+    if [ "$3" == "$ap" ]
+    then
+        ./apriori $1 $2 $4 0
+    elif [ "$3" == "$fp" ]
+    then
+        ./fptree $1 $2 $4 0
+    fi
+fi
--- compile.sh
+++ compile.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-g++ -std=c++11 -O3 -o apriori apriori.cpp
+g++ -std=c++14 -O3 -o apriori apriori.cpp
+g++ -std=c++14 -O3 -o fptree fptree.cpp
+# g++ -std=c++14 -O3 -c apriori.cpp
+# g++ -std=c++14 -O3 -c fptree.cpp
+# g++ -std=c++14 -O3 -c eval_time.cpp
+# g++ -std=c++14 -O3 eval_time.o apriori.o fptree.o -o plot
